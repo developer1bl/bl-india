117b3fdac07601c1337517fafed886df5021c368
developer1bl <160597985+developer1bl@users.noreply.github.com>
2024-05-30 18:03:55 +0530

    add frontend url in backend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://dev.bl-inda.com')],
 
     'allowed_origins_patterns' => [],
 
